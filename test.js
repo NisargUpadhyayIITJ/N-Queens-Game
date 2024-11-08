083430eee1bbb32fbf17d78e34598b0aa9840831
NisargUpadhyayIITJ <141734373+NisargUpadhyayIITJ@users.noreply.github.com>
2024-11-08 18:32:29 +0530

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const str = "#..Q.$Q...$...Q$.Q..$$#.Q..$...Q$Q...$..Q.$$";
-let arr=str.split('#')
-arr=arr.filter((ele)=>(ele!=''))
-let lines=arr[0].split('$').filter((ele)=>(ele!=''))
-console.log(lines);
-
-//hello
